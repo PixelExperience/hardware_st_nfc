303e151afaaac8348398cb9fa91a69e9476bfad0
George Chang <georgekgchang@google.com>
2021-07-30 01:59:33 +0800

    Add a timer to prevent NFCC got stuck in ActiveRW
    When screen off, the power state should switch back to active.
    Start a 5 seconds timer to check if chip can swich back from A_RW
    to Active mode.
    If NFCC got stuck in Active RW mode, reset the chip.
    
    Bug: 194675064
    Bug: 189064168
    Test: manual
    Merged-In: Ia15e000f7ba6a482e5ca69f23cb644eaf0d7b01c
    Change-Id: Ia15e000f7ba6a482e5ca69f23cb644eaf0d7b01c

--- st21nfc/include/halcore.h
+++ st21nfc/include/halcore.h
@@ -55,6 +55,7 @@ typedef enum {
   HAL_WRAPPER_STATE_UPDATE,
   HAL_WRAPPER_STATE_APPLY_CUSTOM_PARAM,
   HAL_WRAPPER_STATE_APPLY_UWB_PARAM,
+  HAL_WRAPPER_STATE_SET_ACTIVERW_TIMER,
 } hal_wrapper_state_e;
 
 /* callback function to communicate from HAL Core with the outside world */
