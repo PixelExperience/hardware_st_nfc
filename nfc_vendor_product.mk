6fccb84f3526a79c88094545067a69f34f307e53
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-11-28 19:28:27 +0000

    st: nfc: Create setup makefile for st nfc hal
    [1] - Based on what Qualcomm did in nxp_opensource_halimpl
    [2] - This will be needed to define the use of st AIDL HAL on future
    
    Change-Id: I87df3dc96410769d3bfae759484dbc0fc9f258a4
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

---
+++ nfc_vendor_product.mk
@@ -0,0 +1,17 @@
+# Enable build support for NFC open source vendor modules
+ST_VENDOR_NFC += android.hardware.nfc@1.2-service.st
+ST_VENDOR_NFC += nfc_nci.st21nfc.default
+
+ifeq ($(strip $(TARGET_USES_ST_NFC)),true)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
+
+PRODUCT_PACKAGES += $(ST_VENDOR_NFC)
+endif
