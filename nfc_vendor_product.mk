31922de466ec37c89946a9451093a640499c95f9
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-11-30 00:51:38 +0000

    st: nfc: aidl: Move vintf_fragments definition to makefile
    [1] - Do not copy to vendor if TARGET_NFC_SKU is being used.
    
    Change-Id: Ib2ee5258460e89ed19871098d24ba8fe9ac571c8
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -22,5 +22,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(NFC_PERMISSIONS_DIR)/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(NFC_PERMISSIONS_DIR)/com.android.nfc_extras.xml
 
+ifeq ($(strip $(TARGET_USES_ST_AIDL_NFC)),true)
+ifneq ($(TARGET_NFC_SKU),)
+else
+DEVICE_MANIFEST_FILE += hardware/st/nfc/aidl/nfc-service-default.xml
+endif
+endif
+
 PRODUCT_PACKAGES += $(ST_VENDOR_NFC)
 endif
